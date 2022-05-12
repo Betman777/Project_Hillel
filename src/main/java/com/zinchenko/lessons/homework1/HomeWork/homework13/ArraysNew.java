a12357e2ad15d7ac452f4ecf321b7c717fbcbf55
Vladimir Zinchenko <Zincvalo5@gmail.com>
2022-05-12 16:47:09 +0300

    new commit six

--- src/main/java/com/zinchenko/lessons/homework1/HomeWork/homework13/ArraysNew.java
+++ src/main/java/com/zinchenko/lessons/homework1/HomeWork/homework13/ArraysNew.java
@@ -3,9 +3,9 @@ package com.zinchenko.lessons.homework1.HomeWork.homework13;
 public class ArraysNew {
 
 
-    public double findAverage(int[][]array) {
+    public double findAverage(int[][] array) {
         int sum = 0;
-        int count=0;
+        int count = 0;
         double average;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
@@ -13,20 +13,21 @@ public class ArraysNew {
                 sum += array[i][j];
             }
         }
-        average =(double) sum / count;
+        average = (double) sum / count;
 
         return average;
     }
 
-    public boolean isSquare(int[][]arraySquare) {
+    public boolean isSquare(int[][] arraySquare) {
         for (int i = 0; i < arraySquare.length; i++) {
             for (int j = 0; j < arraySquare[i].length; j++) {
                 if (arraySquare[i].length != arraySquare[j].length) {
                     return false;
                 }
-                }
-
-            }return true;
+            }
 
         }
+        return true;
+
     }
+}
--- src/test/java/ArraysNewTest.java
+++ src/test/java/ArraysNewTest.java
@@ -7,39 +7,44 @@ import org.junit.jupiter.api.Test;
 
 public class ArraysNewTest {
     ArraysNew arraysNew = new ArraysNew();
-int[][] array = new int[][]{{3, 1, 5}, {5, 8}, {1, 2, 9}};
-int[][] array2 = new int[][]{{3, 7, 5}, {5,4, 9}, {1, 9,7}};
+    int[][] array = new int[][]{{3, 1, 5}, {5, 8}, {1, 2, 9}};
+    int[][] array2 = new int[][]{{3, 7, 5}, {5, 4, 9}, {1, 9, 7}};
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         System.out.println("Before");
         arraysNew = new ArraysNew();
     }
+
     @Test
     public void testAverage() {
         System.out.println("TestAverage");
-        Assertions.assertEquals(4.25 ,arraysNew.findAverage(array));
+        Assertions.assertEquals(4.25, arraysNew.findAverage(array));
     }
+
     @Test
     public void testAverage2() {
         System.out.println("TestAverage2");
-        Assertions.assertEquals(625 ,arraysNew.findAverage(array2));
+        Assertions.assertEquals(625, arraysNew.findAverage(array2));
     }
+
     @Test
-    public void testIsSquare(){
+    public void testIsSquare() {
         System.out.println("TestIsSquare");
         Assertions.assertTrue(arraysNew.isSquare(array));
     }
+
     @Test
-    public void testIsSquare2(){
+    public void testIsSquare2() {
         System.out.println("TestIsSquare2");
         Assertions.assertTrue(arraysNew.isSquare(array2));
     }
 
-@AfterEach
-    public void after(){
-    System.out.println("After");
-    arraysNew = null;
+    @AfterEach
+    public void after() {
+        System.out.println("After");
+        arraysNew = null;
 
-}
+    }
 }
 
