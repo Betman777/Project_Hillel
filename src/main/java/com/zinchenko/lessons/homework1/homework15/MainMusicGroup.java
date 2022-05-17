d8789e447f28b00ad683ab4847514f6578ad452d
Vladimir Zinchenko <Zincvalo5@gmail.com>
2022-05-18 01:54:20 +0300

    new commit eight

--- src/main/java/com/zinchenko/lessons/homework1/homework15/MainMusicGroup.java
+++ src/main/java/com/zinchenko/lessons/homework1/homework15/MainMusicGroup.java
@@ -3,16 +3,19 @@ package com.zinchenko.lessons.homework1.homework15;
 public class MainMusicGroup {
     public static void main(String[] args) {
         PopMusic popMusicGroup = new PopMusic();
+        popMusicGroup.playMusic();
         RockMusic rockMusicGroup = new RockMusic();
+        rockMusicGroup.playMusic();
         ClassicMusic classicMusicGroup = new ClassicMusic();
+        classicMusicGroup.playMusic();
 
 
-        MusicStyles [] array2 = new MusicStyles[]{
-                popMusicGroup,rockMusicGroup,classicMusicGroup
-        };
-        for (MusicStyles musicStyles : array2){
-            musicStyles.playMusic();
-        }
+//        MusicStyles [] array2 = new MusicStyles[]{
+//                popMusicGroup,rockMusicGroup,classicMusicGroup
+//        };
+//        for (MusicStyles musicStyles : array2){
+//            musicStyles.playMusic();
+//        }
 
         MusicStyles [] array = new MusicStyles[]{
                 new PopMusic(),new RockMusic(),new ClassicMusic()
